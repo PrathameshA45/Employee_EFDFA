b148dfe24cf3bf01731cfa9a4f0f3ee516ef6e4d
amrutkar045 <prathameshamrutkar445.com>
2025-08-28 15:54:52 +0530

    Add project files.

---
+++ Employee_EFDFA/Controllers/EmpController.cs
@@ -0,0 +1,72 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.Entity;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using Employee_EFDFA.Models;
+    namespace Employee_EFDFA.Controllers
+    {
+        public class EmpController : Controller
+        {
+          MVCDBEntities dc = new MVCDBEntities();
+            public ViewResult DisplayEmployees()
+            {
+                var Emps = dc.Employees.Where(E => E.Status == true);
+                return View(Emps);
+            }
+            public ViewResult DisplayEmployee(int Eid)
+            {
+                var Emp = dc.Employees.Find(Eid);
+                return View(Emp);
+            }
+            [HttpGet]
+            public ViewResult AddEmployee()
+            {
+                ViewBag.Did = new SelectList(dc.Departments, "Did", "Dname", "Select Departrment");
+                return View();
+            }
+            [HttpPost]
+            public RedirectToRouteResult AddEmployee(Employee Emp)
+            {
+                Emp.Status = true;
+                dc.Employees.Add(Emp);
+                dc.SaveChanges();
+                return RedirectToAction("DisplayEmployees");
+            }
+            [HttpGet]
+            public ViewResult EditEmployee(int Eid)
+            {
+                Employee Emp = dc.Employees.Find(Eid);
+                ViewBag.Did = new SelectList(dc.Departments, "Did", "Dname", Emp.Did);
+                return View(Emp);
+            }
+            [HttpPost]
+            public RedirectToRouteResult UpdateEmployee (Employee Emp)
+            {
+                Emp.Status = true;
+                dc.Entry(Emp).State = EntityState.Modified;
+                dc.SaveChanges();
+                return RedirectToAction("DisplayEmployees");
+            }
+            public ViewResult DeleteEmployee(int Eid)
+            {
+                Employee Emp = dc.Employees.Find(Eid);
+                return View(Emp);
+            }
+            [HttpPost]
+            public RedirectToRouteResult DeleteEmployee(Employee Emp)
+            {
+                //If we want to update the status of employee use the below code:
+                dc.Entry(Emp).State = EntityState.Modified;
+                //If we want to delete the record permanently comment the above statement and un-comment the below:
+                //dc.Entry(Emp).State = EntityState.Deleted;
+                dc.SaveChanges();
+                return RedirectToAction("DisplayEmployees");
+            }
+
+
+        }
+    }
---
+++ Employee_EFDFA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Employee_EFDFA.MvcApplication" Language="C#" %>
