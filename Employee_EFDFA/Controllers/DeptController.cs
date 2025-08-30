c6007cdba2f0e7845ff72efc18dd7054dfabb49d
amrutkar045 <prathameshamrutkar445.com>
2025-08-30 13:26:47 +0530

    Revert "update"
    This reverts commit 9fbb98eecc641940597c172347eb76725ebc867a.

--- Employee_EFDFA/Controllers/DeptController.cs
+++ Employee_EFDFA/Controllers/DeptController.cs
@@ -66,7 +66,7 @@ namespace Employee_EFDFA.Controllers
             var dept = dc.Departments.Find(Did);
             return View(dept); 
         }
-        
+        //DeleteDept Dept
         public RedirectToRouteResult DeleteDept (Department Dept)
         {
             var dept = dc.Departments.Find(Dept.Did);
